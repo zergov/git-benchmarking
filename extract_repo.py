4c158514bd182caf3e0a5c328d3523abe16da201
Jonathan Lalande <lalandej.gg@gmail.com>
2024-09-22 02:18:12 -0400

    add extract commits with pydriller

--- extract_repo.py
+++ extract_repo.py
@@ -1,8 +1,9 @@
 import sqlite3
+from datetime import datetime
 
-def create_database(filename: str):
-    connection = sqlite3.connect(filename)
+from pydriller import Repository
 
+def create_database(connection: sqlite3.Connection):
     cursor = connection.cursor()
     cursor.execute("DROP TABLE IF EXISTS commits;")
     cursor.execute("""
@@ -14,7 +15,32 @@ def create_database(filename: str):
     """)
 
     connection.commit()
+
+
+def dump_database_stats(connection: sqlite3.Connection):
+    cursor = connection.cursor()
+    stats = {
+        "commit_count": cursor.execute("SELECT count(*) FROM commits;").fetchone()[0],
+    }
+    print(stats)
+
+
+def extract_repo_with_pydriller(path: str, since: datetime, to: datetime):
+    repository = Repository(path, since=since, to=to)
+
+    connection = sqlite3.connect(f"db/pydriller_extract.db")
+    create_database(connection)
+    cursor = connection.cursor()
+
+    for commit in repository.traverse_commits():
+        row = (commit.hash, commit.author.name, commit.committer_date)
+        cursor.execute("INSERT INTO commits VALUES (?, ?, ?)", row)
+
+    connection.commit()
+    dump_database_stats(connection)
     connection.close()
 
 
-create_database("db/testing.db")
+t1 = datetime(2010, 1, 1)
+t2 = datetime(2020, 1, 1)
+extract_repo_with_pydriller("./repos/rails_full_clone", t1, t2)
